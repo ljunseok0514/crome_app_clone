5d309fb1e8aa0b52f33d5d7194573b32a47781c4
ljunseok0514 <73566234+ljunseok0514@users.noreply.github.com>
2022-09-01 11:15:10 +0900

    4.5 Saving Username

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSumit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSumit);
-const link = document.querySelector("a");
-link.addEventListener("click", handleLinkClick);
+
+// function handleLinkClick(event) {
+//   event.preventDefault();
+//   console.dir(event);
+// }
+
+// const link = document.querySelector("a");
+// link.addEventListener("click", handleLinkClick);
